e0218a45409296409c677091d7c0a1e01f95cfab
akshay77 <ajakshayjain11@gmail.com>
2023-12-10 23:30:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// import express from "express"
+
+const express = require("express")
+
+const server = express();
+const port = 4000
+
+console.log("akshay jain");
+console.log("aditya jain");
+
+console.log("jai hind");
+
+server.listen(port)
