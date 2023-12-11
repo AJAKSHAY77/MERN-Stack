5ad2fe513a02b6eb1c0dd6ab82cc7f08c48fac6d
akshay77 <ajakshayjain11@gmail.com>
2023-12-11 19:16:04 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-// import express from "express"
+// // import express from "express"
 
-const express = require("express")
+// const express = require("express")
 
-const server = express();
-const port = 4000
+// const server = express();
+// const port = 4000
 
-console.log("akshay jain");
-console.log("aditya jain");
+// console.log("akshay jain");
+// console.log("aditya jain");
 
-console.log("jai hind");
+// console.log("jai hind");
 
-server.listen(port)
+// server.listen(port)
+
+const http = require("http");
+const fs = require("fs");
+
+const index = fs.readFileSync("index.html", "utf-8");
+const data = fs.readFileSync("data.json", "utf-8");
+
+const server = http.createServer((req, res) => {
+  console.log("sever started");
+
+  if(req.url.startsWith("/")){
+      res.setHeader("content-type", "application/json");
+     
+    res.end(index);
+  }
+   if(req.url.startsWith("/demo")) {
+    res.setHeader("content-type", "text/html");
+    res.end(data);
+  } 
+});
+
+server.listen(5000);
